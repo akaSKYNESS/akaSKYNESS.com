02db411b94f6d010b0772dd2fe522b5428fe1d2a
Andrew Kim <akaSKYNESS@users.noreply.github.com>
2014-07-28 21:47:31 -0400

    add fully functional carousel with current slide tracker javascript

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$('.carousel').carousel();
+
+$('.counter').each(function() {
+  var total = $(this).parents('.project').find('.carousel-inner .item').length;
+  $(this).text('1 / ' + total);
+});
+
+$('.carousel').on('slid.bs.carousel', function() {
+  var carouselData = $(this).data('bs.carousel');
+  var currentIndex = carouselData.getItemIndex(carouselData.$element.find('.item.active'));
+  var total = carouselData.$items.length;
+
+  var txt = (currentIndex + 1) + ' / ' + total;
+
+  $(this).parents('.project').find('.counter').text(txt);
+});
